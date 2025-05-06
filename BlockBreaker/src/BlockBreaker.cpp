cc26fb1cb51c4743f6ff482503cb2679b3039d67
MrGuyman99 <165674010+MrGuyman99@users.noreply.github.com>
2025-05-06 18:05:13 -0500

    Beta V0.6!
    Game is functional! Couple of polishing and quality of life enhancements still need to be done though.

---
+++ BlockBreaker/src/BlockBreaker.cpp
@@ -0,0 +1,182 @@
+#include "BlockBreaker.hpp"
+#include<cmath>
+#include<numbers>
+#include<iostream>
+#include<string>
+
+//Constructer Script
+Paddle::Paddle(const char *imagePath, Vector2 imagePosition){
+
+    PaddleTexture = LoadTexture(imagePath);
+    PaddlePosition = imagePosition;
+    
+}
+
+//Deconstructer
+Paddle::~Paddle(){
+
+    UnloadTexture(PaddleTexture);
+
+}
+
+void Paddle::Draw(){
+
+    DrawTextureV(PaddleTexture, PaddlePosition, WHITE);
+    
+}
+
+void Paddle::Update(){
+
+    //Speed Variable (Powerups/adjustabilty)
+    int speed = 8;
+
+    /*Character Controller*/
+    if(IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)){
+        
+        PaddlePosition.x = PaddlePosition.x - speed;
+
+    }
+
+    if(IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)){
+        
+        PaddlePosition.x = PaddlePosition.x + speed;
+
+    }
+
+    //Right Side Escape Prevention
+    if(PaddlePosition.x <= 10){
+
+        PaddlePosition.x = 10;
+
+    }
+
+    //Left Side (Significantly More Complicated lol)
+    if(PaddlePosition.x + 256 >= GetScreenWidth() - 10){
+
+        PaddlePosition.x = GetScreenWidth() - 10 - 256;
+
+    }
+
+}
+
+Ball::Ball(const char *imagePath, Vector2 imagePosition){
+
+    BallTexture = LoadTexture(imagePath);
+    BallPosition = imagePosition;
+
+}
+
+Ball::~Ball() {
+
+    UnloadTexture(BallTexture);
+    UnloadSound(HitSound2);
+}
+
+void Ball::Draw(){
+
+    DrawTextureV(BallTexture, BallPosition, WHITE);
+
+}
+
+void Ball::Update(){
+
+    BallPosition.x = BallPosition.x + speed_x;
+    BallPosition.y = BallPosition.y + speed_y;
+
+    //(48 x 48 is the dimensions of the Ball image)
+    if(BallPosition.y <= 0){
+
+        speed_y = speed_y * -1;
+        PlaySound(HitSound2);
+
+    }
+
+    if(BallPosition.x >= GetScreenWidth() - 48 || BallPosition.x <= 0){
+
+        speed_x = speed_x * -1;
+        PlaySound(HitSound2);
+    }
+
+    if(BallPosition.y > GetScreenHeight()){
+
+        //TODO: Put Game Over Stuff Here
+        BallPosition.x = 640;
+        BallPosition.y = 400;
+
+    }
+    if(IsKeyDown(KEY_R)){
+
+        speed_x = 0;
+        BallPosition.x = 640;
+
+    }
+}
+
+Blocks::Blocks(Texture2D& texture, Vector2 imagePosition){
+
+    BlockPosition = imagePosition;
+    BlockTexture = texture;
+
+}
+
+Blocks::~Blocks(){
+
+    /*
+    Placeholder
+    (Texture is a global texture that gets unloaded)
+    */
+
+}
+
+void Blocks::Draw(){
+
+    if(Destroyed == false){
+
+        DrawTextureV(BlockTexture, BlockPosition, WHITE);
+
+    }
+}
+
+void Blocks::Update(Ball& ball){
+
+    if(Destroyed == false){
+        
+        if(CheckCollisionCircleRec(Vector2{ball.BallPosition.x + 24, ball.BallPosition.y + 24}, 24, 
+            Rectangle{BlockPosition.x, BlockPosition.y, 112, 8}) || CheckCollisionCircleRec(Vector2{ball.BallPosition.x + 24, ball.BallPosition.y + 24}, 24, 
+                Rectangle{BlockPosition.x, BlockPosition.y + 70, 112, 8})){
+
+            ball.speed_y = ball.speed_y * -1;
+        
+            if(ball.speed_y > 0){
+            
+                ball.BallPosition.y = ball.BallPosition.y + 5;
+
+            }
+            else{
+            
+                ball.BallPosition.y = ball.BallPosition.y - 5;
+
+            }
+            
+            //Side Collisions
+            if(CheckCollisionCircleRec(Vector2{ball.BallPosition.x + 24, ball.BallPosition.y + 24}, 24, 
+                Rectangle({BlockPosition.x, BlockPosition.y, 8, 70}))){
+
+                ball.speed_x = ball.speed_x * -1;
+                ball.BallPosition.x = ball.BallPosition.x - 5;    
+
+            }
+            else if(CheckCollisionCircleRec(Vector2{ball.BallPosition.x + 24, ball.BallPosition.y + 24}, 24, 
+                Rectangle({BlockPosition.x + 111, BlockPosition.y, 8, 70}))){
+
+                ball.speed_x = ball.speed_x * -1;
+                ball.BallPosition.x = ball.BallPosition.x + 5;
+            
+            }
+            
+            PlaySound(ball.HitSound2);
+            Destroyed = true;
+
+        }
+    }
+}
---
+++ BlockBreaker/src/BlockBreaker.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include<raylib.h>
+#include<iostream>
+#include<string>
+
+class Paddle{
+
+    public:
+        Paddle(const char* imagePath, Vector2 imagePosition);
+        ~Paddle();
+        void Draw();
+        void Update();
+        Vector2 PaddlePosition;
+
+    private:
+        Texture2D PaddleTexture;
+};
+
+class Ball{
+
+    public:
+        Ball(const char* imagePath, Vector2 imagePosition);
+        ~Ball();
+        void Draw();
+        void Update();
+        Vector2 BallPosition;
+        int speed_y = -8;
+        int speed_x = 8;
+        Sound HitSound2 = LoadSound("Audio/BlockBreakerHit.wav");
+
+    private:
+        Texture2D BallTexture;
+};
+
+class Blocks{
+
+    public:
+        Blocks(Texture2D& texture, Vector2 imagePosition);
+        ~Blocks();
+        void Draw();
+        void Update(Ball& ball);
+        Vector2 BlockPosition;
+
+    private:
+        bool Destroyed = false;
+        Texture2D BlockTexture;
+};  
